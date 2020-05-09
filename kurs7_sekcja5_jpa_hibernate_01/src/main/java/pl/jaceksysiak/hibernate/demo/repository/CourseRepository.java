bfcaea2d4d7640168b531c8ed35b6649a83b796e
Jacek <Jacek@BERLIN>
2020-05-09 07:17:43 +0200

    cc

---
+++ kurs7_sekcja5_jpa_hibernate_01/src/main/java/pl/jaceksysiak/hibernate/demo/repository/CourseRepository.java
@@ -0,0 +1,25 @@
+package pl.jaceksysiak.hibernate.demo.repository;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import pl.jaceksysiak.hibernate.demo.entity.Course;
+
+
+@Repository
+public class CourseRepository {	
+	
+	@Autowired
+	EntityManager em;
+	
+	public Course findById(Long id){
+		return em.find(Course.class, id);
+	}
+	
+	//public Course save(Course course) -> insert or update
+	
+	//public void deleteById(Long id)
+
+}
